d4e8f72a33188aebc8d0e9d8687691212ce48bd5
TamVik <91209683+TamVik@users.noreply.github.com>
2022-08-22 17:09:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let linkedin = document.getElementById("linked-in")
+
+linkedin.addEventListener("click", function() {
+    console.log("clicked")
+    window.open('https://www.linkedin.com/in/viktoria-tamas/')
+})
